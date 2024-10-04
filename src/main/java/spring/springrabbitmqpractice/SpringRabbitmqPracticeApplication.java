4fc97e4c3063cf945c2fbdc4767ba0b860b7ab43
anxi01 <tjdals2893@naver.com>
2024-10-05 02:14:22 +0900

    init

---
+++ src/main/java/spring/springrabbitmqpractice/SpringRabbitmqPracticeApplication.java
@@ -0,0 +1,13 @@
+package spring.springrabbitmqpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqPracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringRabbitmqPracticeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/spring/springrabbitmqpractice/SpringRabbitmqPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springrabbitmqpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqPracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
